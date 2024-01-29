47070668fef2c67e81c138bbb8ae5c355902e22e
nobin-seemymachines <nobin@seemymachines.com>
2024-01-29 11:56:44 +0530

    saga

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+
+import { createStore, applyMiddleware, compose } from "redux";
+import createSagaMiddleware from "redux-saga";
+import { Provider } from "react-redux";
+
+import { reducer } from "./redux";
+import { watcherSaga } from "./sagas";
+
+// create the saga middleware
+const sagaMiddleware = createSagaMiddleware();
+
+// create a redux store with our reducer above and middleware
+let store = createStore(
+  reducer,
+  applyMiddleware(sagaMiddleware)
+);
+
+// run the saga
+sagaMiddleware.run(watcherSaga);
+
+
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+  <Provider store={store}>
     <App />
+  </Provider>,
   </React.StrictMode>
 );
 
